224376d37b7e9578e20b389c561b8b2c1a8e630a
minju0213 <mjni1214@naver.com>
2022-08-07 17:27:17 +0900

    08_07 - 프로필 생성 화면 XML 거의 완성

--- app/src/main/java/com/example/friendsdiary/User_Create.java
+++ app/src/main/java/com/example/friendsdiary/User_Create.java
@@ -2,6 +2,9 @@ package com.example.friendsdiary;
 
 import android.os.Bundle;
 import android.view.View;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,9 +13,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class User_Create extends AppCompatActivity {
 
-    EditText et_user_name;
     TextView btn_mbti[] = new TextView[16];
     int select_mbti_num = 0;
+
+    EditText et_user_name, et_like, et_hate, et_introduction;
+
+    TextView tv_like_length, tv_hate_length, tv_introduction_length;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,6 +26,7 @@ public class User_Create extends AppCompatActivity {
         setContentView(R.layout.user_create);
         init();
         set_mbti_btn();
+        EditTextChanged();
     }
 
     void init() {
@@ -40,6 +47,7 @@ public class User_Create extends AppCompatActivity {
         btn_mbti[14] = findViewById(R.id.btn_ISTJ);
         btn_mbti[15] = findViewById(R.id.btn_ISTP);
     }
+
     void set_mbti_btn() {
         for (int i = 0; i < btn_mbti.length; i++) {
             int finalI = i;
@@ -55,15 +63,18 @@ public class User_Create extends AppCompatActivity {
     }
 
     void select_mbti(int jimin) {
-        if((jimin >= 0 && jimin <= 1) || (jimin >= 8 && jimin <= 9)) {
+        if ((jimin >= 0 && jimin <= 1) || (jimin >= 8 && jimin <= 9)) {
             btn_mbti[jimin].setBackgroundColor(this.getResources().getColor(R.color.mbti_nf_back));
             btn_mbti[jimin].setTextColor(this.getResources().getColor(R.color.mbti_nf_text));
-        } else if((jimin >= 2 && jimin <= 3) || (jimin >= 10 && jimin <= 11)) {
+
+        } else if ((jimin >= 2 && jimin <= 3) || (jimin >= 10 && jimin <= 11)) {
             btn_mbti[jimin].setBackgroundColor(this.getResources().getColor(R.color.mbti_nt_back));
             btn_mbti[jimin].setTextColor(this.getResources().getColor(R.color.mbti_nt_text));
-        } else if((jimin >= 5 && jimin <= 6) || (jimin >= 13 && jimin <= 14)) {
+
+        } else if ((jimin >= 5 && jimin <= 6) || (jimin >= 13 && jimin <= 14)) {
             btn_mbti[jimin].setBackgroundColor(this.getResources().getColor(R.color.mbti_sj_back));
             btn_mbti[jimin].setTextColor(this.getResources().getColor(R.color.mbti_sj_text));
+
         } else {
             btn_mbti[jimin].setBackgroundColor(this.getResources().getColor(R.color.mbti_sp_back));
             btn_mbti[jimin].setTextColor(this.getResources().getColor(R.color.mbti_sp_text));
@@ -74,5 +85,71 @@ public class User_Create extends AppCompatActivity {
         btn_mbti[seojun].setBackgroundColor(this.getResources().getColor(R.color.mbti_default_back));
         btn_mbti[seojun].setTextColor(this.getResources().getColor(R.color.mbti_default_text));
     }
+
+
+    void EditTextChanged() {
+        et_like = (EditText) findViewById(R.id.et_like);
+        tv_like_length = (TextView) findViewById(R.id.tv_like_length);
+
+        et_like.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tv_like_length.setText(s.length() + "/100");   //글자수 TextView에 보여주기.
+            }
+        });
+
+        et_hate = (EditText) findViewById(R.id.et_hate);
+        tv_hate_length = (TextView) findViewById(R.id.tv_hate_length);
+
+        et_hate.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tv_hate_length.setText(s.length() + "/100");   //글자수 TextView에 보여주기.
+            }
+        });
+
+        et_introduction = (EditText) findViewById(R.id.et_introduction);
+        tv_introduction_length = (TextView) findViewById(R.id.tv_introduction_length);
+
+
+        et_introduction.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tv_introduction_length.setText(s.length() + "/100");   //글자수 TextView에 보여주기.
+            }
+        });
+    }
+
+
 }
 
